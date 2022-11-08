b3a7844dc4ff6a0d0bac6dc175313b12e8e4a4b6
murmaty <71284483+murmaty@users.noreply.github.com>
2022-11-08 16:55:40 +0100

    Update set-data-directory.R

--- set-data-directory.R
+++ set-data-directory.R
@@ -1,2 +1,2 @@
 # set-data-directory for da_data_repo!!!
-data_dir="C:/Users/kezdi/Dropbox/bekes_kezdi_textbook/da_data_repo"
+data_dir="C:/Users/muranyim/OneDrive - MediConcept Kft/Dokumentumok/workspace/da_data_repo
